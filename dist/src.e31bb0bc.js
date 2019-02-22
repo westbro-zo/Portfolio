462797b4cc7e9f9fdb3489df0dfd6f7733b055ce
westbro <westbro00@gmail.com>
2019-02-22 18:50:45 +0900

    image 변경

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -253,7 +253,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51715" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57667" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
