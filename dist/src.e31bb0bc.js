a5e8c118dab18b867abcb536a7dbcdb9afbd62fc
westbro <westbro00@gmail.com>
2019-03-01 17:05:34 +0900

    modify content

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -293,7 +293,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57642" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65294" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
