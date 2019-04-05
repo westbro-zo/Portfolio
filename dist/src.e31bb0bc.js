32ca543aa2ab368da948024635ebfa92a0f1e760
westbro <westbro00@gmail.com>
2019-04-05 23:00:44 +0900

    내용 수정

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -293,7 +293,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "0.0.0.0" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62268" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61947" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
